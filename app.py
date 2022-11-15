1ecfcc492a9f0c0dd0e0627e04190548001c7fed
Jared Barojas <jaredbarojas90@gmail.com>
2022-11-15 09:40:08 -0700

    agregar producto

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #todo en español
 from flask import Flask, render_template, request, session, redirect, flash
 import mysql.connector
+import os
 
 
 app = Flask(__name__)
@@ -32,9 +33,28 @@ def index():
 def tienda():
     return render_template('shop.html')
 
-@app.route('/agregar_producto.html')
+@app.route('/agregar_producto.html', methods=['GET','POST'])
 def agregar_producto():
-    return render_template('agregar_producto.html')
+    if request.method=='GET':
+        return render_template('agregar_producto.html')
+    if request.method=='POST':
+        nombre_introducido=request.form['nombre_producto']
+        precio_introducido=str(request.form['precio'])
+        descripcion_introducido=request.form['descripcion']
+        foto_introducida=request.files['foto']
+        foto_ruta = foto_introducida.filename
+        foto_introducida.save(os.path.join('static/img/',foto_ruta))
+        existencia_introducido=str(request.form['existencia'])
+        if request.form['activo']=='on':
+            activo_introducido=True
+        else:
+            activo_introducido=False
+        #print(nombre_introducido,precio_introducido,descripcion_introducido,foto,existencia_introducido,activo_introducido)
+        #nombre,precio,descripcion,foto,existencia,activo
+        query=f'INSERT INTO productos (nombre,precio,descripcion,foto,existencia,activo) VALUES ("{nombre_introducido}",{precio_introducido},"{descripcion_introducido}","{foto_ruta}",{existencia_introducido},{activo_introducido})'
+        cursor.execute(query)
+        return render_template('agregar_producto.html')
+
 
 @app.route('/cart.html')
 def carrito():
